e01dee07ab4b54200a7a058790cbc0406a521870
Vinícius Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2023-01-21 16:28:05 -0300

    create route file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,8 @@
-const passport = require('../config/passport');
+const googleLoginRoute = require('./google-login');
 
 /**
  * @param {import('express').Express} app
  */
 module.exports = (app) => {
-  app.get('/login/federated/google', passport.authenticate('google', {
-    session: false,
-  }));
-  app.get('/oauth2/redirect/google', passport.authenticate('google', {
-    session: false,
-  }), (req, res) => {
-    res.redirect('/');
-  });
-  app.get('/', (_, res) => res.send('ok'));
+  app.use(googleLoginRoute);
 };
